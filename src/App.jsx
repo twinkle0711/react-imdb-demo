0637e269e2cafa03a3f3a533c79af09daa32d378
Twinkle Satekar <satekartwinkle7@gmail.com>
2024-09-26 16:27:46 -0400

    Integrate github pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import Navbar from './components/Navbar';
 import Movies from './components/Movies';
 import Watchlist from './components/Watchlist';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Banner from './components/Banner';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
 
         <Routes>
           <Route path='/' element={<> <Banner /><Movies /> </>}></Route>
+          <Route path='/react-imdb-demo' element={<> <Banner /><Movies /> </>}></Route>
           <Route path='/watchlist' element={<Watchlist />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
