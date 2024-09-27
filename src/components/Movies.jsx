a9be15ae631fba5f80b7a3ac30ab492cc7d54112
Twinkle Satekar <satekartwinkle7@gmail.com>
2024-09-26 23:10:35 -0400

    Implement pagination

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,16 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import Card from './Card'
 import axios from 'axios'
+import Pagination from './Pagination';
 
 function Movies() {
   const [movies, setMovies] = useState([])
+  const [pageNo, setPageNo] = useState(1)
+
+  const handlePrev = () => {
+    if (pageNo === 1) {
+      setPageNo(1)
+    } else {
+      setPageNo(pageNo - 1)
+    }
+  }
+
+  const handleNext = () => {
+    setPageNo(pageNo + 1)
+  }
 
   useEffect(() => {
-    axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=6d2f99282911176e12555e983b1ed98a&language=en-US&page=1')
+    axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=6d2f99282911176e12555e983b1ed98a&language=en-US&page=${pageNo}`)
       .then((res) => {
         setMovies(res.data.results);
       })
-  }, []);
+  }, [pageNo]);
 
   return (
     <div className='p-5'>
@@ -21,10 +35,12 @@ function Movies() {
       <div className='flex flex-row flex-wrap justify-around gap-8'>
         {
           movies.map((movie) => {
-            return <Card poster_path = { movie.poster_path } name = { movie.original_title } />
+            return <Card poster_path={movie.poster_path} name={movie.original_title} />
           })
         }
       </div>
+
+      <Pagination handlePrev={handlePrev} handleNext={handleNext} pageNo={pageNo} />
     </div>
   )
 }
